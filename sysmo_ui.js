3e236b4e652bc9c3297560c9e9750ec03880283d
Joel Van Horn <joel>
2011-12-07 22:45:08 -0500

    added jQuery and blockUI-dependent helper methods that depend on sysmo.js

---
+++ sysmo_ui.js
@@ -0,0 +1,74 @@
+(function(Sysmo, window, document, undefined) {
+  
+  if (!Sysmo) throw Error("Sysmo is required to load Sysmo.UI");
+  
+  var is_loading = false,
+    display_message = true,
+    loading_messages = [],
+    loaded_timer,
+    loading_default_timeout = 250,
+    loading_message = function(message, timeout, callback) {
+      timeout = (timeout==null) ? loading_default_timeout : timeout;
+      clearTimeout(loaded_timer);
+      //if (message) {
+        if (display_message) {
+          var next = function() {
+            callback && callback();
+            display_message = true;
+            var args = loading_messages.shift();
+            args && loading_message.apply(this, args);
+          };
+          if (message) {
+            $("#loading_message")/*.hide()*/.html(message);//.text(message)/*.fadeIn('fast')*/;
+            display_message = false;
+            setTimeout(next, timeout)
+          } else {
+            next();
+          }
+        } else {
+          loading_messages.push([message, timeout, callback]);
+        }
+      //}
+    },
+    loading_resizer = function() {
+      //$('.blockUI.blockPage').centerH();
+      //$('.blockUI.blockPage').centerV();
+      $('.blockUI.blockPage').center();
+    },
+    loaded_finalizer = function(timeout, callback) {
+      return setTimeout(function() {
+        $(window).unbind('resize', loading_resizer);
+        $.unblockUI({ onUnblock: callback });
+        //$("#loading_message").fadeOut('fast');
+        is_loading = false;
+      }, (timeout==null) ? loading_default_timeout : timeout)
+    },
+    loading = function (message, timeout) {
+      
+      if (!is_loading) {
+        $.blockUI();
+        loading_resizer();
+        $(window).resize(loading_resizer);
+      }
+      
+      loading_message(message, timeout);
+      is_loading = true;
+    },
+    loaded = function () {
+      return Sysmo.arrangeArgs(arguments, 
+        String, Number, Function, 
+        function(message, timeout, callback) {
+          console.log("message", message, "timeout", timeout, "callback", callback);
+          
+          loading_message(message, null, function() {
+            loaded_timer = loaded_finalizer(timeout, callback);
+          });
+        });
+    };
+  
+  Sysmo.include('UI', {
+    loading: loading,
+    loaded: loaded
+  });
+
+})(window.Sysmo, window, document);
